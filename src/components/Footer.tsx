458db2e5fc35e9813a7eb138066858be1d3f6425
snowingfox <1503401882@qq.com>
2024-05-31 22:59:16 +0800

    chore: typo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,12 @@ export function Footer() {
   return (
     <div className={'fixed w-full bottom-12 flex justify-center'}>
       <a
-        href="intent://#Intent;package=snowingfox.book.app;end"
+        href="https://xq261aa61x.feishu.cn/file/JNGjbxYFyoswqExUQiBccXKsnxh?from=from_copylink"
         className={
           'bg-primary text-white text-sm rounded-full px-4 py-2 shadow-lg shadow-primary'
         }
       >
-        在App内打开
+        下载小肥书App
       </a>
     </div>
   )
