547d1c992d40f52cd5ee9a4553beecf29645ffca
snowingfox <1503401882@qq.com>
2024-06-19 14:10:50 +0800

    feat: push

---
+++ src/Home.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from './assets/logo.png'
+
+export const Home: React.FC = () => {
+
+    const jumpDownloadURL = (url: string) => {
+        window.location.href = url
+    }
+
+    return (
+        <div className={'w-screen h-screen flex-col bg-white overflow-hidden flex items-center'}>
+            <div className="mt-32">
+                <img src={logo} className={'w-32 h-32'} />
+                <p className="text-center mt-4 text-xl font-bold text-black/70">小肥书</p>
+            </div>
+            <div className="flex-col space-y-2 mt-4 mt-40">
+                <div
+                    className="bg-primary w-80 text-center font-bold rounded-full p-3 text-white"
+                    onClick={() => {
+                        jumpDownloadURL("https://xq261aa61x.feishu.cn/file/IK5fbSt2uoleq2xS5NOc1UKunIh?from=from_copylink")
+                    }}
+                >点我下载Android</div>
+                <div onClick={() => {
+                    jumpDownloadURL("https://testflight.apple.com/join/qTFiYMCl")
+                }} className="w-80 text-center rounded-full p-3 text-primary font-bold bg-white border-primary border-[1px]">下载IOS（testflight)</div>
+            </div>
+        </div>
+
+    )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,12 @@
 import { createHashRouter } from 'react-router-dom'
 import { InnerApp } from './App.tsx'
+import { Home } from './Home.tsx'
 
 export const routes = createHashRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
   {
     path: '/:id',
     element: <InnerApp />,
